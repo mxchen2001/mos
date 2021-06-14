ad8e1f5d3444ae69ca1fbadd397b7dcf85626dfe
Xige Michael Chen <mxchen2001@gmail.com>
2021-06-14 15:18:18 -0500

    vga device

---
+++ OSDEV/64-bit/src/kernel/vga.c
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdint.h>
+
+/* Number of columns and rows on the text display. */
+#define COL_CNT 80
+#define ROW_CNT 25
+
+uint16_t hi;
+
+static void init(void) {
+    /* Already initialized? */
+    static short inited;
+
+    if (!inited) {
+        inited = true;
+    }
+}
